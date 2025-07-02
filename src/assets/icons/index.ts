d17533230a834a48107e8f26036f469671e040df
Sandipani <sandipani@Sandipanis-MacBook-Pro.local>
2025-07-02 12:58:27 +0300

    fix: deprecated option

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,11 @@
 export const icons = Object.fromEntries(
-  Object.entries(import.meta.glob('./*.svg', { eager: true, as: 'url' })).map(
-    ([path, url]) => [path.replace('./', '').replace('.svg', ''), url],
-  ),
+  Object.entries(
+    import.meta.glob("./*.svg", {
+      eager: true,
+      query: "?url",
+      import: "default",
+    }),
+  ).map(([path, url]) => [path.replace("./", "").replace(".svg", ""), url]),
 ) as Record<string, string>;
 
 export type IconName = keyof typeof icons;
